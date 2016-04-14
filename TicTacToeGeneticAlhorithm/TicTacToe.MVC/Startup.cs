186a73c676afb3479438055b38a7cfe4cf0ddc19
Artur.Saharchuk <a.saharchuk@itransition.com>
2016-04-14 12:46:04 +0300

    The project to run application was created.

---
+++ TicTacToeGeneticAlhorithm/TicTacToe.MVC/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicTacToe.MVC.Startup))]
+namespace TicTacToe.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
